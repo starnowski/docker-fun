4b89b45531e70700d6075694fe11282f8f72405e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-09-15 22:19:36 +0200

    #31 - Added script which creates optional user.

---
+++ images/ssh_server/startSSHAndCreateOptionallyUser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -n "$OPTIONAL_SSH_USER"  ]; then
+  echo "Creating user $OPTIONAL_SSH_USE"
+  useradd -m -p Kent "$OPTIONAL_SSH_USER"
+  mkdir -p "/home/$OPTIONAL_SSH_USER/.ssh"
+  ssh-keygen -q -t rsa -N '' -f "/home/$OPTIONAL_SSH_USER/.ssh/id_rsa"
+  cat "/home/$OPTIONAL_SSH_USER/.ssh/id_rsa.pub" >> "/home/$OPTIONAL_SSH_USER/.ssh/authorized_keys"
+fi
+
+/usr/sbin/sshd -D
