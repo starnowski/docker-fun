0d76f94685098a29dc299b2ff9a9f2f4ccefa9de
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-10-12 11:48:39 +0200

    #32 - Escaping passed items. [skip travis]

--- images/ansible_server/ansible_project/run-command-for-items.sh
+++ images/ansible_server/ansible_project/run-command-for-items.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DIRNAME="$(dirname $0)"
-ansible-playbook $DIRNAME/run-command-for-items.yml -e "_command_items=$1" -e "_loop_command=$2"
+echo "Passed items: $1"
+echo "Running command: $2"
+ansible-playbook $DIRNAME/run-command-for-items.yml -e "_command_items=\"$1\"" -e "_loop_command=$2"
