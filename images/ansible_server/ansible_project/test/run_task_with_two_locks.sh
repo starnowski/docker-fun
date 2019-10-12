da7394feb10e0dfaf74dd9726af8b15e283db5c4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-10-12 22:54:06 +0200

    #32 - Added test script which operates on locks. [skip travis]

---
+++ images/ansible_server/ansible_project/test/run_task_with_two_locks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+function waitUntilBarrierWillBeRemoved {
+    checkCount=1
+    timeoutInSeconds=180
+    while : ; do
+        set +e
+        test -e "$1"
+        [[ "$?" -ne 0 && $checkCount -ne $timeoutInSeconds ]] || break
+        checkCount=$(( checkCount+1 ))
+        echo "Waiting $checkCount seconds to release lock $1"
+        sleep 1
+    done
+    set -e
+}
+
+ITEM=$1
+LOCKS_DIR="$2"
+
+if [[ "$ITEM" == "l1" ]] ; then
+    rm -f --preserve-root $LOCKS_DIR/barrier1.lock
+    sleep 7
+    waitUntilBarrierWillBeRemoved "$LOCKS_DIR/barrier2.lock"
+else
+    rm -f --preserve-root $LOCKS_DIR/barrier2.lock
+    sleep 7
+    waitUntilBarrierWillBeRemoved "$LOCKS_DIR/barrier1.lock"
+fi
