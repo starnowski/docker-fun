322973bdaedc20ae869edaa0ce98d02874cf6464
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-10-12 14:08:48 +0200

    #32 - Added "-vvvv" option for debugging [skip travis]

--- images/ansible_server/ansible_project/run-command-for-items.sh
+++ images/ansible_server/ansible_project/run-command-for-items.sh
@@ -2,4 +2,5 @@
 DIRNAME="$(dirname $0)"
 echo "Passed items: $1"
 echo "Running command: $2"
+#ansible-playbook $DIRNAME/run-command-for-items.yml -e "_command_items='$1'" -e "_loop_command='$2'"
 ansible-playbook $DIRNAME/run-command-for-items.yml -e "_command_items='$1'" -e "_loop_command='$2'" -vvvv
